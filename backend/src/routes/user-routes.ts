fe4d345c34de20645fa1a6bc1fb8babc66122c1f
anjing0921 <duyuan0921@gmial.com>
2024-08-27 15:58:11 -0700

    add user-router

---
+++ backend/src/routes/user-routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+
+
+
+
+//Protected API
+const chatRoutes = Router();
+chatRoutes.post(
+  "/new",
+  validate(chatCompletionValidator),
+  verifyToken,
+  generateChatCompletion
+);
+chatRoutes.get("/all-chats", verifyToken, sendChatsToUser);
+chatRoutes.delete("/delete", verifyToken, deleteChats);
+
+export default chatRoutes;
