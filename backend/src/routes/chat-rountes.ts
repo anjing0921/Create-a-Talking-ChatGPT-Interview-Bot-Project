9f2528a1867e9450fc2c395af8bfb061d8fbbf16
anjing0921 <duyuan0921@gmial.com>
2024-08-27 16:06:19 -0700

    creat validators.ts

--- backend/src/routes/chat-rountes.ts
+++ backend/src/routes/chat-rountes.ts
@@ -1,17 +1,17 @@
 import { Router } from "express";
-// import {
-//   getAllUsers,
-//   userLogin,
-//   userLogout,
-//   userSignup,
-//   verifyUser,
-// } from "../controllers/user-controllers.js";
-// import {
-//   loginValidator,
-//   signupValidator,
-//   validate,
-// } from "../utils/validators.js";
-// import { verifyToken } from "../utils/token-manager.js";
+import {
+  getAllUsers,
+  userLogin,
+  userLogout,
+  userSignup,
+  verifyUser,
+} from "../controllers/user-controllers.js";
+import {
+  loginValidator,
+  signupValidator,
+  validate,
+} from "../utils/validators.js";
+import { verifyToken } from "../utils/token-manager.js";
 
 const userRoutes = Router();
 
